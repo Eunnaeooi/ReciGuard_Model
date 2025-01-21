7d8ed9c0eb17f2f2b9a9cf08a6081730949e6137
Eunna <160303427+Eunnaeooi@users.noreply.github.com>
2025-01-21 14:30:21 +0900

    Update config.py
    실시간 db 연동

--- recipe-recommender/app/config.py
+++ recipe-recommender/app/config.py
@@ -1,10 +1,18 @@
-# aws MySQL 연결 정보
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# AWS MySQL 연결 정보
 host = "reciguard-db.czouas0eqnqo.ap-northeast-2.rds.amazonaws.com"
 port = 3306
 user = 'reciguard'
 password = 'tavereciguard'
 database = 'reciguard'
 charset = 'utf8mb4'
+
+# SQLAlchemy 엔진 생성
+DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset={charset}"
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # 하이퍼파라미터
 WEIGHT_CUISINE = 0.6
@@ -15,5 +23,5 @@ TIME_WEIGHT = 0.3
 DECAY_RATE = 7
 SIMILARITY_THRESHOLD = 0.4
 
-CONTENT_WEIGHT = 0.9  
-COLLAB_WEIGHT = 0.1   
+CONTENT_WEIGHT = 0.9
+COLLAB_WEIGHT = 0.1
