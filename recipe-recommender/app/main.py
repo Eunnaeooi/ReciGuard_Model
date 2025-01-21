dcfb94bcf70f1d7bcc514e82ff92516d3c1df6f1
Eunna <160303427+Eunnaeooi@users.noreply.github.com>
2025-01-21 14:32:09 +0900

    Update main.py
    실시간 db 연동

--- recipe-recommender/app/main.py
+++ recipe-recommender/app/main.py
@@ -1,30 +1,26 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
-import pandas as pd
+from sqlalchemy.orm import Session
+from app.data_loader import load_data
 from app.hybrid_recommender import HybridRecommender
 from app.recipe_filter import RecipeFilter
-from app.data_loader import load_data
+from app.config import SessionLocal
 
 app = FastAPI()
 
-# 데이터 로드
-Ingredient, Recipe_Ingredient, User_Ingredient, Recipe, User_Cuisine, User_FoodType, User_CookingStyle, User_Scrap = load_data()
-
-'''
-# csv 테스트 전처리!
-from datetime import datetime
-User_Scrap['created_at'] = User_Scrap['created_at'].apply(
-    lambda x: datetime.now() if x == 'NOW()' else pd.to_datetime(x)
-)
-'''
-
+# DB 세션 생성 함수
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 # RecipeFilter 초기화
-recipe_filter = RecipeFilter(rules_file_path="app/rules.pkl")  
+recipe_filter = RecipeFilter(rules_file_path="app/rules.pkl")
 
 # HybridRecommender 초기화
 hybrid_recommender = HybridRecommender()
-
 
 # 서버 상태 확인 엔드포인트
 @app.get("/")
@@ -35,11 +31,13 @@ def root():
 class RecommendationRequest(BaseModel):
     user_id: int
 
-
 # 추천 요청 엔드포인트
 @app.post("/recommend")
-def recommend_recipe(request: RecommendationRequest):
+def recommend_recipe(request: RecommendationRequest, db: Session = Depends(get_db)):
     try:
+        # 데이터 로드
+        Ingredient, Recipe_Ingredient, User_Ingredient, Recipe, User_Cuisine, User_FoodType, User_CookingStyle, User_Scrap = load_data(db)
+
         # 레시피 필터링
         Filtered_Recipe = recipe_filter.filter_recipes(
             user_id=request.user_id,
@@ -48,7 +46,7 @@ def recommend_recipe(request: RecommendationRequest):
             Recipe_Ingredient=Recipe_Ingredient,
             Recipe=Recipe
         )
-        
+
         # 하이브리드 추천 실행
         recommendation = hybrid_recommender.recommend(
             request.user_id,
@@ -61,4 +59,3 @@ def recommend_recipe(request: RecommendationRequest):
         return {"recipe_id": recommendation}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
