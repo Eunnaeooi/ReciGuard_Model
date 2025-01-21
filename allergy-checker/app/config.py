ffdf8bd314cfc2bde7dd5422fc9d2495b89e12b4
Eunna <160303427+Eunnaeooi@users.noreply.github.com>
2025-01-21 13:35:04 +0900

    Update config.py
    실시간 db 연동

--- allergy-checker/app/config.py
+++ allergy-checker/app/config.py
@@ -1,10 +1,20 @@
-# aws MySQL 연결 정보
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# AWS MySQL 연결 정보
 host = "reciguard-db.czouas0eqnqo.ap-northeast-2.rds.amazonaws.com"
 port = 3306
 user = 'reciguard'
 password = 'tavereciguard'
 database = 'reciguard'
 charset = 'utf8mb4'
+
+# SQLAlchemy 엔진 생성
+DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset={charset}"
+engine = create_engine(DATABASE_URL, echo=True)
+
+# 세션 관리
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # 하이퍼파라미터
 SIMILARITY_THRESHOLD = 50
